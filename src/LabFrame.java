084108311295839c3a56073afb485e1b79025a70
Niklas <bknjniklas@gmail.com>
2015-11-27 19:37:22 +0100

    Swing stuff...

---
+++ src/LabFrame.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Niklas on 2015-11-27.
+ */
+import java.awt.*;
+import javax.swing.*;
+
+public class LabFrame extends JPanel {
+    public LabFrame(int width, int height) {
+        JFrame f = new JFrame();
+        setLayout(new GridLayout(width, height));
+        f.setSize(100*width, 100*height);
+        f.setVisible(true);
+        f.add(this);
+    }
+}
--- src/LabSolver.java
+++ src/LabSolver.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import javax.swing.*;
+
 public class LabSolver {
 
     public static void main(String[] args) {
